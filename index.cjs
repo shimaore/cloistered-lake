141447cc52075252579f7e2c79b6eeb6ed567e8c
Stephane Alnet <stephane@shimaore.net>
2022-01-13 10:24:21 +0100

    replace rollup with esbuild

--- index.cjs
+++ index.cjs
@@ -1,127 +1,110 @@
-'use strict';
-
-Object.defineProperty(exports, '__esModule', { value: true });
-
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __markAsModule = (target) => __defProp(target, "__esModule", { value: true });
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __reExport = (target, module2, copyDefault, desc) => {
+  if (module2 && typeof module2 === "object" || typeof module2 === "function") {
+    for (let key of __getOwnPropNames(module2))
+      if (!__hasOwnProp.call(target, key) && (copyDefault || key !== "default"))
+        __defProp(target, key, { get: () => module2[key], enumerable: !(desc = __getOwnPropDesc(module2, key)) || desc.enumerable });
+  }
+  return target;
+};
+var __toCommonJS = /* @__PURE__ */ ((cache) => {
+  return (module2, temp) => {
+    return cache && cache.get(module2) || (temp = __reExport(__markAsModule({}), module2, 1), cache && cache.set(module2, temp), temp);
+  };
+})(typeof WeakMap !== "undefined" ? /* @__PURE__ */ new WeakMap() : 0);
+var cloistered_lake_exports = {};
+__export(cloistered_lake_exports, {
+  Lake: () => Lake,
+  always: () => always,
+  bigNaturals: () => bigNaturals,
+  concurrentMap: () => concurrentMap,
+  empty: () => empty,
+  equals: () => equals,
+  from: () => from,
+  merge: () => merge,
+  now: () => now,
+  periodic: () => periodic,
+  sleep: () => sleep,
+  throwError: () => throwError
+});
 var Always, BigNaturals, ConcurrentMap, Empty, From, LakeAsyncIterator, Merge, Now, Periodic, ThrowError, concurrentize, isIdentical, mergeArray;
-
 var sleep = function(timeout, value = void 0) {
   return new Promise(function(resolve) {
-    return setTimeout((function() {
+    return setTimeout(function() {
       return resolve(value);
-    }), timeout);
+    }, timeout);
   });
 };
-
 isIdentical = function(a, b) {
   return a === b;
 };
-
-// # Lake(stream|iterator)
-
-  // An Async Iterator that behaves as a Readable Stream and supports Monadic Event
-// Stream patterns, using only native operators.
-LakeAsyncIterator = class LakeAsyncIterator {
+LakeAsyncIterator = class LakeAsyncIterator2 {
   constructor(stream1) {
     this.stream = stream1;
   }
-
-  // It can be used as a proxy for the original (sync or async) iterator, turning it
-  // into an async iterator.
   async next() {
-    return (await this.stream.next());
+    return await this.stream.next();
   }
-
-  // It also is an async iterable, which means it can be turned into a stream.
   [Symbol.asyncIterator]() {
-    return new LakeAsyncIterator(this);
+    return new LakeAsyncIterator2(this);
   }
-
-  // ## .map(transform)
-
-    // Applies a (sync or async) transformation function to each element in the stream.
   map(f) {
     var stream;
-    ({stream} = this);
-    return Lake((async function*() {
+    ({ stream } = this);
+    return Lake(async function* () {
       var chunk;
       for await (chunk of stream) {
         yield f(chunk);
       }
-    })());
+    }());
   }
-
-  // ## .concurrentMap(atmost,transform)
-
-    // Applies an async transformation function to each element in the stream, running
-  // at most `atmost` instances concurrently.
-
-    // The ordering of the elements is not guaranteed, since it will depend on the
-  // evaluation time of the async `transform` function.
   concurrentMap(atmost, fun) {
     return concurrentMap(this.stream, atmost, fun);
   }
-
-  // ## .constant(value)
-
-    // Transform this stream into a stream that produces the `value` for each element
-  // the original stream produces.
-  constant(v) {
+  constant(v2) {
     return this.map(function() {
-      return v;
+      return v2;
     });
   }
-
-  // ## .filter(fun)
-
-    // Only forward stream values for which the (sync or async) `fun` function returns
-  // (a Promise for) a truthy value.
   filter(f) {
     var stream;
-    ({stream} = this);
-    return Lake((async function*() {
+    ({ stream } = this);
+    return Lake(async function* () {
       var chunk;
       for await (chunk of stream) {
-        if ((await f(chunk))) {
+        if (await f(chunk)) {
           yield chunk;
         }
       }
-    })());
+    }());
   }
-
-  // ## .skipRepeats()
-  // ## .skipRepeats(isEqual)
-
-    // When successive values are identical, only the first one is propagated.
-
-    // Optionally, a (sync or async) comparison function might be provided to compare
-  // using a different criteria than `===`; it should return `true` if its two
-  // arguments are considered identical.
   skipRepeats(eq = isIdentical) {
     var stream;
-    ({stream} = this);
-    return Lake((async function*() {
+    ({ stream } = this);
+    return Lake(async function* () {
       var chunk, last;
       for await (chunk of stream) {
-        if (!(await eq(chunk, last))) {
+        if (!await eq(chunk, last)) {
           yield chunk;
         }
         last = chunk;
       }
-    })());
+    }());
   }
-
-  // ## .first(n)
-  // ## .take(n)
-
-    // Only propagates the first `n` elements in the stream.
-
-    // BigInt are used internally; `n` might be a integer or a BigInt.
   first(max) {
     var n, stream;
-    ({stream} = this);
+    ({ stream } = this);
     n = 0n;
     max = BigInt(max);
-    return Lake((async function*() {
+    return Lake(async function* () {
       var chunk;
       if (n >= max) {
         return;
@@ -132,24 +115,16 @@ LakeAsyncIterator = class LakeAsyncIterator {
         }
         yield chunk;
       }
-    })());
+    }());
   }
-
   take(n) {
     return this.first(n);
   }
-
-  // ## .skip(n)
-
-    // Skips the first `n` elements in the stream and only start propagating after the
-  // `n`-th element has been received.
-
-    // BigInt are used internally; `n` might be a integer or a BigInt.
   skip(n) {
     var stream;
-    ({stream} = this);
+    ({ stream } = this);
     n = BigInt(n);
-    return Lake((async function*() {
+    return Lake(async function* () {
       var chunk;
       if (n <= 0) {
         return;
@@ -159,31 +134,23 @@ LakeAsyncIterator = class LakeAsyncIterator {
           yield chunk;
         }
       }
-    })());
+    }());
   }
-
-  // ## .delay(timeout)
-
-    // Insert a delay of `timeout` milliseconds between each received element.
   delay(timeout) {
     var stream;
-    ({stream} = this);
-    return Lake((async function*() {
+    ({ stream } = this);
+    return Lake(async function* () {
       var chunk;
       for await (chunk of stream) {
         await sleep(timeout);
         yield chunk;
       }
-    })());
+    }());
   }
-
-  // ## .startWith(otherStream)
-
-    // Concatenates the otherStream with this stream.
   startWith(another) {
     var stream;
-    ({stream} = this);
-    return Lake((async function*() {
+    ({ stream } = this);
+    return Lake(async function* () {
       var chunk;
       for await (chunk of another) {
         yield chunk;
@@ -191,16 +158,12 @@ LakeAsyncIterator = class LakeAsyncIterator {
       for await (chunk of stream) {
         yield chunk;
       }
-    })());
+    }());
   }
-
-  // ## .continueWith(otherStream)
-
-    // Contatenates this stream then the otherStream.
   continueWith(another) {
     var stream;
-    ({stream} = this);
-    return Lake((async function*() {
+    ({ stream } = this);
+    return Lake(async function* () {
       var chunk;
       for await (chunk of stream) {
         yield chunk;
@@ -208,40 +171,26 @@ LakeAsyncIterator = class LakeAsyncIterator {
       for await (chunk of another) {
         yield chunk;
       }
-    })());
+    }());
   }
-
-  // ## .forEach(func)
-  // ## .tap(func)
-
-    // Executes the (sync or async) `func` function for each element in the stream.
-  // The stream is unmodified but might be delayed by the execution time of `func`.
-  // The stream will fail if `func` fails or rejects.
   forEach(f) {
     var stream;
-    ({stream} = this);
-    return Lake((async function*() {
+    ({ stream } = this);
+    return Lake(async function* () {
       var chunk;
       for await (chunk of stream) {
         await f(chunk);
         yield chunk;
       }
-    })());
+    }());
   }
-
   tap(f) {
     return this.forEach(f);
   }
-
-  // ## .ap(funs)
-
-    // Apply a stream of (sync or async) functions to this stream.
-
-    // Elements of this stream are dropped until `funs` provides a function.
   ap(funs) {
     var stream;
-    ({stream} = this);
-    return Lake((async function*() {
+    ({ stream } = this);
+    return Lake(async function* () {
       var chunk, f, index, ref, x;
       f = null;
       ref = mergeArray([stream, funs]);
@@ -250,26 +199,22 @@ LakeAsyncIterator = class LakeAsyncIterator {
         switch (index) {
           case 0:
             if (f != null) {
-              yield (await f(chunk));
+              yield await f(chunk);
             }
             break;
           case 1:
             f = chunk;
         }
       }
-    })());
+    }());
   }
-
-  // ## .switchLatest()
-
-    // Outputs the data from the latest stream in this stream-of-streams.
   switchLatest() {
     var stream;
     stream = this;
-    return Lake((async function*() {
+    return Lake(async function* () {
       var chunk, current, index;
       current = mergeArray([stream]);
-      while ([chunk, index] = (await current.next())) {
+      while ([chunk, index] = await current.next()) {
         switch (index) {
           case 0:
             current = mergeArray([stream, chunk]);
@@ -278,84 +223,46 @@ LakeAsyncIterator = class LakeAsyncIterator {
             yield chunk;
         }
       }
-    })());
+    }());
   }
-
-  // ## .reduce(reducer,accumulator)
-
-    // Using a (sync or async) `reducer` function which accepts the latest value of the
-  // accumulator and a new value, returns the final value of the accumulator.
   async reduce(f, a) {
     var chunk, stream;
-    ({stream} = this);
+    ({ stream } = this);
     for await (chunk of stream) {
-      a = (await f(a, chunk));
+      a = await f(a, chunk);
     }
     return a;
   }
-
-  // ## .run()
-
-    // Consumes this stream, throwing away values; returns a Promise.
-
-    // The Promise will reject if the stream fails for any reason.
   async run() {
     var chunk, stream;
-    ({stream} = this);
+    ({ stream } = this);
     for await (chunk of stream) {
+      false;
     }
   }
-
-  // ## .last()
-
-    // Consumes this stream, returning its last value (or undefined if no value was
-  // produced) inside a Promise.
   async last() {
     var chunk, last, stream;
-    ({stream} = this);
+    ({ stream } = this);
     last = void 0;
     for await (chunk of stream) {
       last = chunk;
     }
     return last;
   }
-
-  // ## .equals(otherStream)
-  // ## .equals(otherStream,isEqual)
-
-    // Consumes two streams; returns a Promise that is true if both stream yield
-  // the same values.
-
-    // The optional `isEqual` (sync or async) comparison function should return true if
-  // its two arguments are considered equal.
   equals(otherStream, eq = isIdentical) {
     return equals(this, otherStream, eq);
   }
-
 };
-
 var Lake = function(stream) {
   return new LakeAsyncIterator(stream);
 };
-
-// # Merge stream
-
-// Based on https://stackoverflow.com/questions/50585456/how-can-i-interleave-merge-async-iterables
-// The main difference is that we rotate the sources list in order to help
-// Promise.race() make progress on all the streams, not just the first one.
-
-// Takes iterators or async iterators, returns an asyncIterator
-
-//* @param streams: asyncIterator[]
-//* @return asyncIterator
-mergeArray = async function*(streams) {
+mergeArray = async function* (streams) {
   var queueNext, sources, srcs, value, winner;
   queueNext = async function(e) {
-    e.result = null; // Release previous one as soon as possible
-    e.result = (await e.stream.next());
+    e.result = null;
+    e.result = await e.stream.next();
     return e;
   };
-  // Map the generators to source objects in a map, get and start their first iteration.
   sources = new Map(streams.map(function(stream, index) {
     return [
       stream,
@@ -367,28 +274,20 @@ mergeArray = async function*(streams) {
     ];
   }));
   srcs = Array.from(sources.values());
-  // While we still have any sources, race the current promise of the sources we have left
   while (sources.size) {
-    winner = (await Promise.race(srcs));
-    // Completed the sequence?
+    winner = await Promise.race(srcs);
     if (winner.result.done) {
-      // Yes, drop it from sources
       sources.delete(winner.stream);
       srcs = Array.from(sources.values());
     } else {
-      // No, grab the value to yield and queue up the next
-      ({value} = winner.result);
+      ({ value } = winner.result);
       sources.set(winner.stream, queueNext(winner));
-      // Then yield the value and the index of the stream it came from
-      // (the index in the index in the original `streams` parameter array).
       yield [value, winner.index];
     }
-    // Rotate the sources (forcing Promise.race to round-robin over them)
     srcs.unshift(srcs.pop());
   }
 };
-
-Merge = async function*(...streams) {
+Merge = async function* (...streams) {
   var chunk, ref, x;
   ref = mergeArray(streams);
   for await (x of ref) {
@@ -396,200 +295,123 @@ Merge = async function*(...streams) {
     yield chunk;
   }
 };
-
 var merge = function(...streams) {
   return Lake(Merge(...streams));
 };
-
-// ## Concurrent execution
-
-// From a stream that generates Promises, keep at most N concurrent
-// Promises underway; generates a new stream that returns the values in
-// non-deterministic order
-
-//* @param stream: AsyncIterable<Promise>
-//* @param atmost : integer
-concurrentize = async function*(stream, atmost, fun) {
-  var awaitNext, done, index, key, nextKey, pick, pool;
-  pool = new Map();
+concurrentize = async function* (stream, atmost, fun) {
+  var awaitNext, completed, done, index, key, nextKey, pick, pool;
+  pool = /* @__PURE__ */ new Map();
   index = 0n;
   done = false;
+  completed = 0n;
   nextKey = function() {
     index += 1n;
     return index.toString();
   };
-  awaitNext = async function(key, next) {
+  awaitNext = async function(key2, next) {
     var result;
-    result = (await next);
+    result = await next;
     if (!result.done) {
-      result.value = (await fun(result.value));
+      result.value = await fun(result.value);
     }
-    return {key, result};
+    return { key: key2, result };
   };
   while (true) {
-    // While there is still room in the pool, keep consuming values from the source.
     while (!done && pool.size < atmost) {
       key = nextKey();
       pool.set(key, awaitNext(key, stream.next()));
     }
-    // Return once the pool has been flushed
     if (pool.size === 0) {
       return;
     }
-    // The stream will fail if any of the Promises fail.
-    pick = (await Promise.race(pool.values()));
+    pick = await Promise.race(pool.values());
     pool.delete(pick.key);
     if (pick.result.done) {
       done = true;
     } else {
+      completed++;
       yield pick.result.value;
     }
   }
 };
-
-ConcurrentMap = async function*(stream, atmost, fun) {
+ConcurrentMap = async function* (stream, atmost, fun) {
   var ref, value;
   ref = concurrentize(stream, atmost, fun);
   for await (value of ref) {
     yield value;
   }
 };
-
 var concurrentMap = function(stream, atmost, fun) {
   return Lake(ConcurrentMap(stream, atmost, fun));
 };
-
-// # empty()
-
-// Builds a stream that finishes immediately.
-Empty = function() {};
-
+Empty = function() {
+};
 var empty = function() {
   return Lake(Empty());
 };
-
-// # always(v)
-
-// Builds a stream that continously generates the value.
-Always = function*(v) {
+Always = function* (v2) {
   while (true) {
-    yield v;
+    yield v2;
   }
 };
-
 var always = function() {
   return Lake(Always(v));
 };
-
-// # bigNaturals()
-
-// Builds a stream that enumerates all positive BigInt values, starting at 0
-// and incrementing by 1 at each step.
-BigNaturals = function*() {
+BigNaturals = function* () {
   var n;
   n = 0n;
   while (true) {
     yield n++;
   }
 };
-
 var bigNaturals = function() {
   return Lake(BigNaturals());
 };
-
-// # periodic(period)
-// # periodic(period,value)
-
-// Builds a stream that generates a new element with the provided value (or
-// undefined if no value is provided) and generates similarly every `period`
-// milliseconds thereafter.
-Periodic = async function*(period, value = void 0) {
+Periodic = async function* (period, value = void 0) {
   while (true) {
     yield value;
     await sleep(period);
   }
 };
-
 var periodic = function(period) {
   return Lake(Periodic(period));
 };
-
-// # now(v)
-
-// Builds a stream that only produces once, with the value provided.
-Now = function*(v) {
-  return (yield v);
+Now = function* (v2) {
+  return yield v2;
 };
-
-var now = function(v) {
-  return Lake(Now(v));
+var now = function(v2) {
+  return Lake(Now(v2));
 };
-
-// # throwError(e)
-
-// Builds a stream that stops immediately with the provided error.
-ThrowError = function*(error) {
+ThrowError = function* (error) {
   throw error;
+  return yield void 0;
 };
-
 var throwError = function(e) {
   return Lake(ThrowError(e));
 };
-
-// # from(iterable)
-
-// From any iterable, generates a new "Lake" stream (described above).
-
-// The iterable might be an Array, an iterator, an AsyncIterator,
-// a [ReadableStream](https://nodejs.org/dist/latest/docs/api/stream.html#readablesymbolasynciterator), …
-
-// Use Node.js' [`events.on(emitter,eventName)`](https://nodejs.org/dist/latest/docs/api/events.html#eventsonemitter-eventname-options)
-// to create an AsyncIterator from an event-emitter.
-From = async function*(a) {
-  var v;
-  for await (v of a) {
-    yield v;
+From = async function* (a) {
+  var v2;
+  for await (v2 of a) {
+    yield v2;
   }
 };
-
 var from = function(a) {
   return Lake(From(a));
 };
-
-// # equals(streamA,streamB)
-// # equals(streamA,streamB,isEqual)
-
-// From two Lake instances or two iterators, returns a boolean Promise indicating
-// whether the two suites are identical.
-
-// The optional (sync or async) `isEqual` function should return true to indicate
-// that its two arguments are considered identical.
 var equals = async function(A, B, eq = isIdentical) {
   var a, b;
   while (true) {
-    a = (await A.next());
-    b = (await B.next());
+    a = await A.next();
+    b = await B.next();
     if (a.done && b.done) {
       return true;
     }
     if (a.done !== b.done) {
       return false;
     }
-    if (!(await eq(a.value, b.value))) {
+    if (!await eq(a.value, b.value)) {
       return false;
     }
   }
 };
-
-exports.Lake = Lake;
-exports.always = always;
-exports.bigNaturals = bigNaturals;
-exports.concurrentMap = concurrentMap;
-exports.empty = empty;
-exports.equals = equals;
-exports.from = from;
-exports.merge = merge;
-exports.now = now;
-exports.periodic = periodic;
-exports.sleep = sleep;
-exports.throwError = throwError;
+module.exports = __toCommonJS(cloistered_lake_exports);
