bfb7f8f65f0625237e660e10eaca9f9709d8cec9
Stephane Alnet <stephane@shimaore.net>
2022-01-22 20:23:48 +0100

    correct TS definitions

--- index.d.ts
+++ index.d.ts
@@ -1,38 +1,41 @@
+type I<T> = AsyncIterable<T> | Iterable<T>
+type P<T> = Promise<T> | T
+
 declare class LakeAsyncIterator<T> {
-  constructor(stream:AsyncIterable<T>|Iterable<T>);
+  constructor(stream:I<T>);
   // is an Async Generator
   next(): Promise<{ done: boolean, value: T }>;
   // is an AsyncIterable
   [Symbol.asyncIterator](): LakeAsyncIterator<T>
-  map<U>( f: (v:T) => U ): LakeAsyncIterator<U>
-  concurrentMap<U>( atmost: number, f: (v:T) => U ): LakeAsyncIterator<U>
+  map<U>( f: (v:T) => P<U> ): LakeAsyncIterator<U>
+  concurrentMap<U>( atmost: number, f: (v:T) => P<U> ): LakeAsyncIterator<U>
   constant(v:T): LakeAsyncIterator<T>
-  filter(f: (v:T) => boolean): LakeAsyncIterator<T>
-  skipRepeast(isEqual?: (a:T,b:T) => Promise<boolean> | boolean): LakeAsyncIterator<T>
-  first(n:number|BigInt): LakeAsyncIterator<T>
-  take(n:number|BigInt): LakeAsyncIterator<T>
-  skip(n:number|BigInt): LakeAsyncIterator<T>
+  filter(f: (v:T) => P<boolean>): LakeAsyncIterator<T>
+  skipRepeast(isEqual?: (a:T,b:T) => P<T>): LakeAsyncIterator<T>
+  first(n:number|bigint): LakeAsyncIterator<T>
+  take(n:number|bigint): LakeAsyncIterator<T>
+  skip(n:number|bigint): LakeAsyncIterator<T>
   delay(timeout:number): LakeAsyncIterator<T>
-  startWith(otherStream: AsyncIterable<T> | Iterable<T>): LakeAsyncIterator<T>
-  continueWith(otherStream: AsyncIterable<T> | Iterable<T>): LakeAsyncIterator<T>
-  forEach(f: (v:T) => void): LakeAsyncIterator<T>
-  tap(f: (v:T) => void): LakeAsyncIterator<T>
-  ap<U>(funs:AsyncIterable<(a:T) => U>|Iterable<(a:T) => U>): LakeAsyncIterator<U>
+  startWith(otherStream: I<T>): LakeAsyncIterator<T>
+  continueWith(otherStream: I<T>): LakeAsyncIterator<T>
+  forEach(f: (v:T) => P<void>): LakeAsyncIterator<T>
+  tap(f: (v:T) => P<void>): LakeAsyncIterator<T>
+  ap<U>(funs:I<(a:T) => P<U>>): LakeAsyncIterator<U>
   switchLatest(): LakeAsyncIterator<T>
-  reduce<U>( reducer: (acc:U,value:T) => U, initialValue: T ): AsyncGenerator<U>
+  reduce<U>( reducer: (acc:U,value:T) => P<U>, initialValue: U ): Promise<U>
   run(): Promise<void>
   last(): Promise<T|undefined>
-  equals(otherStream:AsyncIterable<T>,isEqual?: (a:T,b:T) => Promise<boolean> | boolean): AsyncGenerator<T>
+  equals(otherStream:I<T>,isEqual?: (a:T,b:T) => P<boolean>): Promise<boolean>
 }
 export function sleep<T>(timeout: number, value?: T): Promise<T>;
-export function Lake<T>(stream: AsyncIterable<T> | Iterable<T>): LakeAsyncIterator<T>;
+export function Lake<T>(stream: I<T>): LakeAsyncIterator<T>;
 export function merge(...streams: any[]): any;
 export function concurrentMap(stream: any, atmost: any, fun: any): any;
 export function empty<T>(): LakeAsyncIterator<T>;
 export function always<T>(): LakeAsyncIterator<T>;
-export function bigNaturals(): LakeAsyncIterator<BigInt>;
+export function bigNaturals(): LakeAsyncIterator<bigint>;
 export function periodic<T>(period: T): LakeAsyncIterator<T>;
 export function now<T>(v: T): LakeAsyncIterator<T>;
 export function throwError<T,E>(e: E): LakeAsyncIterator<T>;
-export function from<T>(a: AsyncIterable<T> | Iterable<T>): LakeAsyncIterator<T>;
-export function equals(A: any, B: any, eq?: any): Promise<boolean>;
+export function from<T>(a: I<T>): LakeAsyncIterator<T>;
+export function equals<T>(A: I<T>, B: I<T>, eq?: (a:T,b:T) => P<boolean>): Promise<boolean>;
---
+++ test-ts/test.ts
@@ -0,0 +1,74 @@
+import {Lake} from '..'
+Lake( process.stdin )
+import {pipeline} from 'stream'
+import * as fs from 'fs'
+import {bigNaturals} from '..'
+pipeline(
+  bigNaturals().skip(1).map( x => x*x ).first(1000).map( x => `${x}\n` ),
+  // This will create a file containing the first thousand squares
+  fs.createWriteStream('thousand-squares.txt'),
+  console.error
+)
+import {merge,from} from '..'
+const BigIntegers = merge(
+  // will enumerate 1, 2, 3, 4, …
+  bigNaturals().skip(1),
+  // will enumerate 0, -1, -2, -3, …
+  bigNaturals().map( x => -x ),
+)
+const firstTwentyIntegers = from(
+  [0,-1,1,-2,2,-3,3,-4,4,-5,5,-6,6,-7,7,-8,8,-9,9,-10].map(BigInt)
+)
+import test from 'ava'
+test('Compute first twenty integers', async t => {
+  t.true(await
+    BigIntegers
+    .first(20)
+    .equals( firstTwentyIntegers )
+  )
+})
+const Sum = (a:bigint,v:bigint) => a+v
+test('Compute the sum of the first 1000 squares', async t => {
+  t.is( 333_833_500n, await
+    bigNaturals()
+    .skip(1)
+    .map( x => x*x )
+    .first(1000n)
+    .reduce(Sum, 0n)
+  )
+})
+import {sleep} from '..'
+const squareMicroService = async (x:bigint) => {
+  await sleep(1)
+  return x*x
+}
+test('Compute the sum of the first 1000 squares using concurrentMap', async t => {
+  console.time('with concurrentMap')
+  t.is( 333_833_500n, await
+    bigNaturals()
+    .skip(1)
+    .concurrentMap(100,squareMicroService)
+    .first(1000n)
+    .reduce(Sum, 0n)
+  )
+  console.timeEnd('with concurrentMap')
+})
+test('Compute the sum of the first 1000 squares without concurrentMap', async t => {
+  console.time('without concurrentMap')
+  t.is( 333_833_500n, await
+    bigNaturals()
+    .skip(1)
+    .map( squareMicroService )
+    .first(1000n)
+    .reduce(Sum, 0n)
+  )
+  console.timeEnd('without concurrentMap')
+})
+test('Retrieve the 14th BigInt', async t => {
+  t.is(14n as bigint | undefined, await
+    bigNaturals()
+    .skip(1) // skip 0
+    .first(14)
+    .last()
+  )
+})
